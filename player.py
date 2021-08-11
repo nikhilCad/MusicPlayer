60e0ca9332af3baf68f0b640851ca6dd670f3db8
nikhilcad <nikhilcaddilac@gmail.com>
2021-08-11 16:59:55 +0530

    Bare minimum starting point

--- player.py
+++ player.py
@@ -1,3 +1,20 @@
 #import pysimplegui
 #do magical stuff
 #profit
+
+import PySimpleGUI as sg
+
+layout = [
+[sg.T("Hello"*10)]
+
+]
+
+window = sg.Window("Window layout", layout)
+
+while True:
+    event, values = window.read()
+
+    if event == sg.WINDOW_CLOSED or event == 'Quit':
+        break
+
+window.close()
