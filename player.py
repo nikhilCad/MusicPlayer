f269236ff8e85a31295cf9fe6b942014393b9779
nikhilcad <nikhilcaddilac@gmail.com>
2021-08-11 17:33:24 +0530

    Folders can be choosen

--- player.py
+++ player.py
@@ -5,16 +5,29 @@
 import PySimpleGUI as sg
 
 layout = [
-[sg.T("Hello"*10)]
+            [sg.T("")],
+            [
+                sg.Text("Choose a folder: "), 
+                sg.Input(key="-IN2-" ,change_submits=True), 
+                sg.FolderBrowse(key="-IN-")
+            ],
+            [sg.Button("Submit")
+            ]
+        ]
 
-]
+###Building Window
+window = sg.Window('My File Browser', layout, size=(600,150))
 
-window = sg.Window("Window layout", layout)
+def play(path):
+    #We have the folder now do something with it
+    print(path)
 
 while True:
     event, values = window.read()
 
-    if event == sg.WINDOW_CLOSED or event == 'Quit':
+    if event == sg.WIN_CLOSED or event=="Exit":
         break
-
+    elif event == "Submit":
+        play(values["-IN-"])
+    
 window.close()
